dfdd0764efaa9ca007ccd181e0e9c39eb1b53333
Jayden Dickinson <jaydend93@gmail.com>
2026-04-07 20:42:45 +1000

    Added src files

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from dotenv import load_dotenv
+from src.NameParser.generate import clsGenerateNames
+
 import os
 
 load_dotenv()
 
 api_key = os.getenv("API_KEY")
 
+
+try:
+    names = clsGenerateNames(api_key)
+    if(names.generate(5)):
+        for name in names._list:
+            print(name) # Prints five random names. 
+            details=names.details(name) # Returns all information on the name generated
+except:
+    print("An exception occurred")
